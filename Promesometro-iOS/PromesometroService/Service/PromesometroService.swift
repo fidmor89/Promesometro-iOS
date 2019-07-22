0d36a056f9d48a42b1919d14e215c7a1da15e735
Fidel Esteban Morales Cifuentes <fidmor.89@outlook.com>
2019-07-21 18:46:26 -0600

    Add `request<T>(route: PromesometroRouter, decoder: JSONDecoder = JSONDecoder()) -> Future<T> where T: Decodable` to `PromesometroService`

--- Promesometro-iOS/PromesometroService/Service/PromesometroService.swift
+++ Promesometro-iOS/PromesometroService/Service/PromesometroService.swift
@@ -6,7 +6,9 @@
 //  Copyright © 2019 Fidmor.89. All rights reserved.
 //
 
+import Alamofire
 import Foundation
+import PromisedFuture
 
 open class PromesometroService: PromesometroWebService {
 
@@ -26,5 +28,29 @@ open class PromesometroService: PromesometroWebService {
     public convenience init(withURL url: URL) {
         baseURL = url
         self.init()
+    }
+
+    /// Makes a request using Alamofire default instance.
+    ///
+    /// - Parameters:
+    ///   - route: Rote to request
+    ///   - decoder: optional decoder for custom object decoding.
+    /// - Returns: Future with promised response of T.type
+    @discardableResult
+    internal func request<T>(route: PromesometroRouter,
+                             decoder: JSONDecoder = JSONDecoder()) -> Future<T> where T: Decodable {
+        return Future(operation: { completion in
+            AF.request(route)
+                .validate()
+                .responseDecodable(decoder: decoder, completionHandler: { (response: DataResponse<T>) in
+                    print("From: \(route.path) Response: \(response.result)")
+                    switch response.result {
+                    case .success(let value):
+                        completion(.success(value))
+                    case .failure(let error):
+                        completion(.failure(error))
+                    }
+                })
+        })
     }
 }
