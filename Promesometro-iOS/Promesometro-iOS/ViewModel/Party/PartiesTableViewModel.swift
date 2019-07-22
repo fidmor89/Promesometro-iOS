d09f0568921a0d4cfc385604cd2bca0eef472694
Fidel Esteban Morales Cifuentes <fidmor.89@outlook.com>
2019-07-21 21:27:27 -0600

    Add PartiesTableViewModel.swift and PartiesTableViewController + project structure

---
+++ Promesometro-iOS/Promesometro-iOS/ViewModel/Party/PartiesTableViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  PartiesTableViewModel.swift
+//  Promesometro-iOS
+//
+//  Created by Fidel Esteban Morales Cifuentes on 7/21/19.
+//  Copyright © 2019 Fidmor.89. All rights reserved.
+//
+
+import Foundation
