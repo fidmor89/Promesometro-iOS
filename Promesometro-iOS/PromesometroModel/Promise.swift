849ba2eca25d674c6b4c70466f4217163583a64c
Fidel Esteban Morales Cifuentes <fidmor.89@outlook.com>
2019-07-21 18:01:39 -0600

    Add PromesometroModel and `Promise` struct

---
+++ Promesometro-iOS/PromesometroModel/Promise.swift
@@ -0,0 +1,15 @@
+//
+//  Promise.swift
+//  PromesometroModel
+//
+//  Created by Fidel Esteban Morales Cifuentes on 7/21/19.
+//  Copyright © 2019 Fidmor.89. All rights reserved.
+//
+
+import Foundation
+
+public struct Promise {
+    var name: String?
+
+    public init () { }
+}
